94912849c7224afcb857d7eac110ad3d45d34243
Alfredo Oltra <aoltra@gmail.com>
2023-05-05 16:57:22 +0200

    feat: arranque automático de odoo al arrancar el contenedor

---
+++ odoo/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -v PASSWORD_FILE ]; then
+    PASSWORD="$(< $PASSWORD_FILE)"
+fi
+
+# En caso de que no se hay adefinido en envoriment de docker los parámetros de la bse de datos
+# los asigna a partir de DB_PORT_5432_TCP_ADDR, DB_PORT_5432_TCP_PORT
+# DB_ENV_POSTGRES_USER y DB_ENV_POSTGRES_PASSWORD y en caso de que no estén 
+# los toma por defecto
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+
+exec /runodoo.sh "$@"
--- odoo/runodoo.sh
+++ odoo/runodoo.sh
@@ -1,1 +1,25 @@
-python3 -m debugpy --listen 5678 /usr/bin/odoo --dev=all 
+#!/bin/bash
+
+# Abort script at first error, when a command exits with non-zero status 
+set -e
+
+# DB_ARGS almacena los argumentos del CLI de odoo relativos a la base de datos
+DB_ARGS=()
+function check_config() {
+    param="$1"
+    value="$2"
+    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
+        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
+    fi;
+    DB_ARGS+=("--${param}")
+    DB_ARGS+=("${value}")
+}
+# comprueba si está definidos en odoo.conf y si lo están coge su valor
+check_config "db_host" "$HOST"
+check_config "db_port" "$PORT"
+check_config "db_user" "$USER"
+check_config "db_password" "$PASSWORD"
+
+# ejecuta odoo a través del módulo debugpy para poder ser depurado con adjutando los módulos
+# $@ es una lista con los parámetros que entran a este script 
+exec python3 -m debugpy --listen 5678 /usr/bin/odoo "$@" "${DB_ARGS[@]}" --dev=all 
